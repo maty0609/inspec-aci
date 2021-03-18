b554e588714a8f5e1fb81a6d887b66f32027d1ae
Matyas <mprokop@natilik.com>
2021-03-18 12:03:50 +0100

    Adding filter contract feature

--- test/dev/aci_dev_entry.rb
+++ test/dev/aci_dev_entry.rb
@@ -9,21 +9,11 @@
 # This is not needed when being loaded as a gem; but when doing
 # plugin development, you may need it.  Either way, it's harmless.
 
-
-
 libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
-
-lib = File.expand_path('/usr/local/rvm/gems/ruby-2.7.2/gems/acirb-1.2.1.0/lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-
-lib = File.expand_path('/usr/local/rvm/gems/ruby-2.7.2/gems/nokogiri-1.11.1-x86_64-linux/lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-
-lib = File.expand_path('/usr/local/rvm/gems/ruby-2.7.2/gems/websocket-1.2.9/lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 require_relative 'plugin.rb'
 require_relative '../../libraries/aci_application_epg.rb'
 require_relative '../../libraries/aci_contract.rb'
+require_relative '../../libraries/aci_filter.rb'
 require_relative '../../libraries/aci_subject.rb'
---
+++ test/integration/aci_filter.rb
@@ -0,0 +1,3 @@
+describe aci_filter(name:'tf-test',tenant:'tf-test-client') do
+  it { should exist }
+end
